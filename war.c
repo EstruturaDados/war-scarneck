e9d4d1aefc93cff5a5ee22c7104531e23a71e93f
scarneck <66283772+scarneck@users.noreply.github.com>
2026-03-22 02:08:41 +0000

    NIVEL AVENTUREIRO - Muita Gambiarra

--- war.c
+++ war.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define NUM_TERR 5
 #define TAM_STR 50
 
-typedef struct {
+struct Territorio {
     char nome[TAM_STR];
     char cor[TAM_STR];
     int tropas;
-} Territorio;
+};
+
+struct Players{
+    struct Territorio *atacante;
+    struct Territorio *defensor;
+};
 
 void bufferCln(){
     int c;
@@ -20,7 +27,8 @@ void interface(){
     printf("              Cadastrando os %d territorios iniciais.\n", NUM_TERR);
     printf("=================================================================\n");
 }
-void territoriosInput(Territorio territorio[]){ //Aprendi com GPT que precisa de [] e não &
+
+void territoriosInput(struct Territorio territorio[]){ //Aprendi com GPT que precisa de [] e não &
     int i = 0;
     while (i < NUM_TERR){
         printf("\n***     Cadastre o territorio %d     ***\n\n", i+1);
@@ -33,7 +41,7 @@ void territoriosInput(Territorio territorio[]){ //Aprendi com GPT que precisa de
         scanf("%d", &territorio[i].tropas);
         bufferCln();
 
-        printf("------------------------------------------------------\n");
+        printf("-----------------------------------------------------------------\n");
 
         territorio[i].nome[strcspn(territorio[i].nome, "\n")] = '\0';
         territorio[i].cor[strcspn(territorio[i].cor, "\n")] = '\0';
@@ -41,33 +49,136 @@ void territoriosInput(Territorio territorio[]){ //Aprendi com GPT que precisa de
     }
     printf("                cadastro inicial completo!\n\n");
 }
-void territoriosOutput(Territorio territorio[]){
+void territoriosOutput(struct Territorio territorio[]){
     printf("=================================================================\n");
     printf("                    ESTADO DO MAPA ATUAL\n");
     printf("=================================================================\n");
 
     for (int i = 0; i < NUM_TERR; i++){
-        printf("TERRITORIO %d\n", i+1);
-        printf("    - Nome do territorio: %s\n", territorio[i].nome);
-        printf("    - Dominado por: Exercito %s\n", territorio[i].cor);
-        printf("    - Tropas: %d\n", territorio[i].tropas);
-        printf("------------------------------------------------------\n");
+        printf("#%d -> %s (Exercito: %s, Tropas: %d)\n",i+1, territorio[i].nome, territorio[i].cor, territorio[i].tropas);
+        printf("-----------------------------------------------------------------\n");
     }
 
 }
+struct Players attackPhase(struct Territorio territorio[]){
+    printf("\n=================================================================\n");
+    printf("                        FASE DE ATAQUE\n");
+    printf("=================================================================\n");
+    
+    do{
+        
+        printf("Escolha o territorio atacante (de 1 a %d ou 0 para sair): ", NUM_TERR);
+        int opt;
+        scanf("%d", &opt);
+        bufferCln();
+        
+        struct Players *player;
+        player = (struct Players*)malloc(sizeof(struct Players));
+        
+        if ((opt<=NUM_TERR) && (opt >=1) && (opt!=0)){
+            player -> atacante = &territorio[opt-1];
+            
+            do{
+                printf("escolha o territorio defensor (de 1 a %d): ", NUM_TERR);
+                int opt2;
+                scanf("%d",&opt2);
+                bufferCln();
+                if ((opt2<=NUM_TERR) && (opt2 >=1) && (opt2!=opt)){
+                    player -> defensor = &territorio[opt2-1];
+                    return *player;
+                }else if(opt2 == opt){
+                    printf("Selecione um defensor diferente do atacante.\n");
+                    continue;
+                }else{
+                    printf("Opcao invalida, tente novamente.\n");
+                    continue;
+                }
+            }while(1);
 
-// Função principal
+        }else if(opt == 0){
+            printf("Saindo...");
+            break;
+        }else{
+            printf("Opcao invalida, tente novamente.\n");
+            continue;
+        }
+    }while(1);
+    struct Players vazio = {NULL, NULL};
+    return vazio;
+}
+void battle(struct Players players){
+    printf("\n=================================================================\n");
+    printf("                    RESULTADO DA BATALHA\n");
+    printf("=================================================================\n");
+    
+    int dAtk = (rand() % 6)+1; 
+    int dDef = (rand() % 6)+1; 
+
+    printf("O atacante rolou um dado e tiro: %d\n", dAtk);
+    printf("O defensor rolou um dado e tiro: %d\n", dDef);
+
+    struct Territorio *vencedor;
+    struct Territorio *perdedor;
+    if(dAtk > dDef){
+        vencedor = players.atacante;
+        perdedor = players.defensor;
+
+        perdedor -> tropas--;
+        if(perdedor->tropas <= 0){
+            strcpy(perdedor->cor, vencedor->cor); //ChatGPT ...
+            perdedor->tropas = 1;
+        }
+
+    }else if(dDef > dAtk){
+        vencedor = players.defensor;
+        perdedor = players.atacante;
+        perdedor->tropas--;
+        if(perdedor->tropas <= 0){
+            strcpy(perdedor->cor, vencedor->cor); //ChatGPT ...
+            perdedor->tropas = 1;
+        }
+    }else{
+        printf("Houve empate.\n");
+        printf("pressione ENTER para continuar.\n");
+        bufferCln();
+        getchar();
+        return; 
+    }
+    printf("VITORIA DA %s! e %s perdeu 1 tropa\n", vencedor->nome, perdedor->nome);
+
+    printf("pressione ENTER para ir para o próximo turno.\n");
+    bufferCln();
+    getchar();
+}
+
+
 int main(){
-    Territorio territorio[NUM_TERR];
+    struct Territorio *territorio;
+    territorio = (struct Territorio*) calloc(NUM_TERR, sizeof(struct Territorio));
 
     //Interface inicial
     interface();
 
     // coletando informações dos territorios
     territoriosInput(territorio);
-    
-    // imprimindo territorios
-    territoriosOutput(territorio);
+
+    srand(time(NULL));
+
+    do{
+        // imprimindo territorios
+        territoriosOutput(territorio);
+        
+        // escolhendo o atacante e o defensor
+        struct Players players = attackPhase(territorio);
+
+        if (players.atacante == NULL || players.defensor == NULL){ //garantindo encerramento
+            break;
+        }else{ // rodando dados
+            battle(players);
+        }
+        
+    }while(1);
+
 
     return 0;
 }
